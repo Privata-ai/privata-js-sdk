f402a40595455208fdee4f6b550430f5c3cf4cda
Pedro Cruz <pedrocrvz@gmail.com>
2020-04-03 13:33:04 +0100

    Add sandbox environment option

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,23 +1,23 @@
 import * as firebase from 'firebase'
 
-// Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: 'AIzaSyDwAIdHtTi94JiG4NDhyRgXrz3dk3RIT8E',
-  authDomain: 'blockbird-data-apps-local-dev.firebaseapp.com',
-  databaseURL: 'https://blockbird-data-apps-local-dev.firebaseio.com',
-  projectId: 'blockbird-data-apps-local-dev',
-  storageBucket: 'blockbird-data-apps-local-dev.appspot.com',
-  messagingSenderId: '962487972446',
-  appId: '1:962487972446:web:f4ef453b612892f3603acf',
-  measurementId: 'G-6QK9PY1TF5',
+var firebaseConfigSandBox  = {
+  apiKey: "AIzaSyDO-JXjcrO9x5sSX30mLQdSpu3_r7yI9gY",
+  authDomain: "blockbird-data-apps-staging.firebaseapp.com",
+  databaseURL: "https://blockbird-data-apps-staging.firebaseio.com",
+  projectId: "blockbird-data-apps-staging",
+  storageBucket: "blockbird-data-apps-staging.appspot.com",
+  messagingSenderId: "930371487629",
+  appId: "1:930371487629:web:0161868047c691df75dfc4",
+  measurementId: "G-PNK4RC89T6"
 }
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfigSandBox, 'sandbox')
+//firebase.initializeApp(firebaseConfigProduction, 'production')
 
-export async function initialize(dbId: string, dbSecret: string) {
+export async function initialize(dbId: string, dbSecret: string, sandbox: boolean) {
   dbId = dbId + '@blockbird.ventures'
   await firebase
+    .app(sandbox ? 'sandbox' : 'production')
     .auth()
     .signInWithEmailAndPassword(dbId, dbSecret)
     .catch(function (error) {
@@ -29,6 +29,6 @@ export async function initialize(dbId: string, dbSecret: string) {
   return
 }
 
-export async function getIdToken(): Promise<string> {
-  return await firebase.auth().currentUser.getIdToken()
+export async function getIdToken(sandbox: boolean): Promise<string> {
+   return await firebase.app(sandbox ? 'sandbox' : 'production').auth().currentUser.getIdToken()
 }
